39d03eb9e9ebd980b9cf7c7b1a998d826d07a47c
jessicafarias <jessica.farias.rosado@gmail.com>
2020-12-23 01:15:44 -0600

    Adding images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import './reset.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './Jessica.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import kasra from './images/kasra.jpg'
+import html from './images/html_css.png'
 import Ruby from './images/Ruby_certification.png'
+import ror from './images/rubyonrails_certification.png'
+import * as helper from './style_helpers.js'
+import newsweek from './images/newsweek.jpg'
+import twitter from './images/twitter_clone.jpg'
+import directory from './images/school.jpg'
+import scaffold from './images/socialmedia.jpg'
+import ceneval from './images/ceneval.png'
+import './Jessica2.js'
+//import Jessica2.js
 
 
-const img = (imgId, src, ...styles) => {
-  const imagen = document.getElementById(imgId);
-  imagen.src = src;
-  imagen.classList.add(...styles);
-};
+helper.img("Ruby", Ruby,'');
+helper.img("kasra", kasra,'');
+helper.img("htmlcss", html,'');
 
-img("Ruby", Ruby,'');
-
+helper.img("ror", ror,'');
+helper.img("newsweek",newsweek);
+helper.img("twitter", twitter);
+helper.img("directory", directory);
+helper.img("scaffold", scaffold);
+helper.img("ceneval", ceneval);
 
 
