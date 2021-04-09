eeff2105427f37174ef048852aebe3ce43534b52
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-04-09 12:39:51 -0500

    docs: Create new empty react-app

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# jessicafarias.github.io
--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import './reset.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import './Jessica.css';
-import kasra from './images/kasra.jpg'
-import html from './images/html_css.png'
-import Ruby from './images/Ruby_certification.png'
-import ror from './images/rubyonrails_certification.png'
-import * as helper from './style_helpers.js'
-import newsweek from './images/newsweek.jpg'
-import twitter from './images/twitter_clone.jpg'
-import directory from './images/school.jpg'
-import scaffold from './images/socialmedia.jpg'
-import ceneval from './images/ceneval.png'
-import forecast from './images/Weather.jpg'
-import game from './images/computer.png'
-import restaurant from './images/57297.jpg'
-import jes from './images/jessica.png'
-import './Jessica2.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-helper.img("Ruby", Ruby,'');
-helper.img("kasra", kasra,'');
-helper.img("htmlcss", html,'');
-helper.img("ror", ror,'');
-helper.img("newsweek",newsweek);
-helper.img("twitter", twitter);
-helper.img("directory", directory);
-helper.img("scaffold", scaffold);
-helper.img("ceneval", ceneval);
-
-helper.img("restaurant",restaurant);
-helper.img("forecast",forecast);
-helper.img("jess", jes);
-helper.img("game",game);
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
